ee8262bfb4f937ba7cd92dcef996924461f6d35e
Shantanu Mahajan <shantanumhjn@gmail.com>
2017-04-30 13:12:52 +0530

    lecture 19, part 5 - Controller As Syntax

--- lecture-19/app.js
+++ lecture-19/app.js
@@ -16,32 +16,37 @@
 
   ChildController1.$inject = ["$scope"];
   function ChildController1($scope) {
-    console.log("$scope.parentValue: ", $scope.parentValue);
-    console.log("CHILD $scope: ", $scope);
-
-    $scope.parentValue = 5;
-    console.log("*** CHANGED: $scope.parentValue = 5 ***");
-    console.log("$scope.parentValue: ", $scope.parentValue);
-    console.log("$scope: ", $scope);
-
-    console.log("$scope.pc.parentValue: ", $scope.pc.parentValue);
-    $scope.pc.parentValue = 5;
-    console.log("*** CHANGED: $scope.pc.parentValue = 5 ***");
-    console.log("$scope.pc.parentValue: ", $scope.pc.parentValue);
-    console.log("$scope: ", $scope);
-
-    console.log("$scope.$parent.parentValue: ", $scope.$parent.parentValue);
+    // console.log("$scope.parentValue: ", $scope.parentValue);
+    // console.log("CHILD $scope: ", $scope);
+    //
+    // $scope.parentValue = 5;
+    // console.log("*** CHANGED: $scope.parentValue = 5 ***");
+    // console.log("$scope.parentValue: ", $scope.parentValue);
+    // console.log("$scope: ", $scope);
+    //
+    // console.log("$scope.pc.parentValue: ", $scope.pc.parentValue);
+    // $scope.pc.parentValue = 5;
+    // console.log("*** CHANGED: $scope.pc.parentValue = 5 ***");
+    // console.log("$scope.pc.parentValue: ", $scope.pc.parentValue);
+    // console.log("$scope: ", $scope);
+    //
+    // console.log("$scope.$parent.parentValue: ", $scope.$parent.parentValue);
   }
 
   // ** Controller As syntax
-  ParentController2.$inject = ["$scope"];
-  function ParentController2($scope) {
-
+  // using the As syntax, the controller is a property in $scope
+  // so we need to not define anything on the scope directly
+  // we can define properties on the Controller itself 
+  function ParentController2() {
+    var parent = this;
+    parent.value = 1;
   }
 
   ChildController2.$inject = ["$scope"];
   function ChildController2($scope) {
-
+    var child = this;
+    child.value = 5;
+    console.log("ChildController2 $scope: ", $scope);
   }
 
 })();
