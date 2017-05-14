9a5290e2760b56de3b33076d5fcb81b1e6c75e5d
Shantanu Mahajan <shantanumhjn@gmail.com>
2017-05-14 21:47:48 +0530

    lecture 37 - controllers in router state

---
+++ lecture-37/src/routes.js
@@ -0,0 +1,35 @@
+(function () {
+'use strict';
+
+// states can have controllers too
+// and they are defined just as in directives
+// either as string (as syntax)
+// or function value
+
+angular.module('ShoppingList')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // *** Set up UI states ***
+  $stateProvider
+
+  // Home page
+  .state('home', {
+    url: '/',
+    templateUrl: 'src/shoppinglist/templates/home.template.html'
+  })
+
+  // Premade list page
+  .state('mainList', {
+    url: '/main-list',
+    templateUrl: 'src/shoppinglist/templates/main-shoppinglist.template.html',
+    controller: 'MainShoppingListController as mainList'
+  });
+}
+
+})();
