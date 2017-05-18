de1b0f3a7cc475d4ab3e8d72bd1796508f8f3416
Shantanu Mahajan <shantanumhjn@gmail.com>
2017-05-18 21:13:18 +0530

    added styling on the items page

--- assignment4/src/menu/items.component.js
+++ assignment4/src/menu/items.component.js
@@ -3,8 +3,21 @@
   angular.module('MenuApp')
     .component('items', {
       templateUrl: 'src/menu/templates/items.template.html',
+      controller: ItemComponentController,
       bindings: {
         items: '<'
       }
     });
+
+  function ItemComponentController () {
+    var $ctrl = this;
+
+    $ctrl.replace = function ($index) {
+      if ($ctrl.items[$index].description) {
+        var temp = $ctrl.items[$index].name;
+        $ctrl.items[$index].name = $ctrl.items[$index].description;
+        $ctrl.items[$index].description = temp;
+      }
+    };
+  }
 })();
